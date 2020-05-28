c974391b6bf4782922c42f67db19ec270672e5c8
Chacko <chackop@gmail.com>
2020-05-28 15:54:15 +0100

    locator strategy

--- src/test/java/Locator_Strategies.java
+++ src/test/java/Locator_Strategies.java
@@ -1,11 +1,14 @@
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.URL;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class Sessions {
+public class Locator_Strategies {
 
     private static final String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.apk";
     private static final String APPIUM = "http://localhost:4723/wd/hub";
@@ -21,6 +24,7 @@ public class Sessions {
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("app", APP);
         driver = new AndroidDriver(new URL(APPIUM), caps);
+        try { Thread.sleep(3000); } catch (Exception ign) {}
     }
 
     @After
@@ -32,6 +36,8 @@ public class Sessions {
 
     @Test
     public void test() {
-        System.out.println("Here's our test!");
+        driver.findElement(MobileBy.AccessibilityId("Login Screen"));
+        List<WebElement> elements = driver.findElements(MobileBy.AccessibilityId("Login Screen"));
+        System.out.println(elements.size());
     }
 }
